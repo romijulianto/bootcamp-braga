f2b61a278f92c2c4df640a824c915341791507cb
romijulianto <romyjulians@gmail.com>
2021-04-16 16:52:36 +0700

    menambahkan fastify static dan nodemon

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const path = require('path')
+
+// Register plugins
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/', // optional: default '/'
+});
+// second plugin
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public/assets'),
+  prefix: '/assets', // optional: default '/'
+  decorateReply: false // the reply decorator has been added by the first plugin registration
+});
+// third plugin
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public/forms'),
+  prefix: '/forms', // optional: default '/'
+  decorateReply: false // the reply decorator has been added by the first plugin registration
+});
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+  return reply.sendFile('index.html'); // serving path.join(__dirname, 'public', 'myHtml.html') directly
+});
 
 // Declare a route
 fastify.get('/apaya', async (request, reply) => {
     return { hello: 'apaya' }
-  })
+  });
 
 // Run the server!
 const start = async () => {
